98abe8ad85ba0a3f8792157cac6ccb3598c32158
FortinMaxence <fortin@et.esiea.fr>
2022-02-25 00:30:19 +0100

    Initialisation du projet

---
+++ src/test/java/com/esiea/deltablog/DeltaBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiea.deltablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
