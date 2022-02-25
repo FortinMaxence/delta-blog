0f391bb402de056c31e5e2c8ef7404a63445f95d
FortinMaxence <fortin@et.esiea.fr>
2022-02-25 01:37:22 +0100

    Initialisation front-end

---
+++ blog-front/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+      <Header />
+
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ blog-front/src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="App-header">
+            <h1 className="App-title">Delta Blog | BODIN Paul & FORTIN Maxence</h1>
+        </header>
+    )
+}
